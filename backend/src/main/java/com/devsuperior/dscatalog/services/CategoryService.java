9ed1f9f14b7b350345b24267043ded751e9babd4
brunoclaumari <brunoclaumari@gmail.com>
2020-11-30 18:36:37 -0300

    Database seeding

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
@@ -11,9 +12,13 @@ import com.devsuperior.dscatalog.repositories.CategoryRepository;
 @Service
 public class CategoryService {
 	
+	//autowired faz a injeção de dependencia automática
 	@Autowired
 	private CategoryRepository repository;
 	
+	//o 'Transactional' define que o método ocorrerá
+	//em uma transação
+	@Transactional(readOnly = true)
 	public List<Category> findAll(){
 		return repository.findAll();
 	}
