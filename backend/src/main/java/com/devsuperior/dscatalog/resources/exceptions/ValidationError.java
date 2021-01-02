0e60b3a8a034128492a2325154488c58c8acd515
brunoclaumari <brunoclaumari@gmail.com>
2021-01-02 16:42:05 -0300

    Custom ConstraintValidator

--- backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ValidationError.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ValidationError.java
@@ -3,7 +3,8 @@ package com.devsuperior.dscatalog.resources.exceptions;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ValidationError extends StandardError {
+public class ValidationError extends StandardError {	
+	private static final long serialVersionUID = 1L;
 	
 	private List<FieldMessage> errors = new ArrayList<>();
 
