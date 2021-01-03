6133ba8f60bf294028a0a17eb4c2afda6286a6fb
brunoclaumari <brunoclaumari@gmail.com>
2021-01-03 19:11:14 -0300

    External configuration

--- backend/src/main/java/com/devsuperior/dscatalog/config/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/SecurityConfig.java
@@ -1,23 +1,9 @@
 package com.devsuperior.dscatalog.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
-public class SecurityConfig {	
+public class SecurityConfig {		
 	
-	@Bean
-	public JwtAccessTokenConverter accessTokenConverter() {
-		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-		tokenConverter.setSigningKey("MY-JWT-SECRET");
-		return tokenConverter;
-	}
-
-	@Bean
-	public JwtTokenStore tokenStore() {
-		return new JwtTokenStore(accessTokenConverter());
-	}
 		
 }
